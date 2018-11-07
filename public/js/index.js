4491eca31b2c22ab221f0a75b78af524dc1b1c80
purxiz <functionofn@gmail.com>
2018-11-07 15:28:01 -0800

    fix phaser example

--- public/js/index.js
+++ public/js/index.js
@@ -14,22 +14,22 @@ var config = {
     preload: preload,
     create: create
   }
-};
+}
 
-var game = new Phaser.Game(config);
+var game = new Phaser.Game(config)
 
 function preload() {
-  this.load.setBaseURL('http://labs.phaser.io');
+  this.load.setBaseURL('http://labs.phaser.io')
 
-  this.load.image('sky', 'assets/skies/space3.png');
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-  this.load.image('red', 'assets/particles/red.png');
+  this.load.image('sky', 'assets/skies/space3.png')
+  this.load.image('logo', 'assets/sprites/phaser3-logo.png')
+  this.load.image('red', 'assets/particles/red.png')
 }
 
 function create() {
-  this.add.image(400, 300, 'sky');
+  this.add.image(400, 300, 'sky')
 
-  var particles = this.add.particles('red');
+  var particles = this.add.particles('red')
 
   var emitter = particles.createEmitter({
     speed: 100,
@@ -38,13 +38,13 @@ function create() {
       end: 0
     },
     blendMode: 'ADD'
-  });
+  })
 
-  var logo = this.physics.add.image(400, 100, 'logo');
+  var logo = this.physics.add.image(400, 100, 'logo')
 
-  logo.setVelocity(100, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
+  logo.setVelocity(100, 200)
+  logo.setBounce(1, 1)
+  logo.setCollideWorldBounds(true)
 
-  emitter.startFollow(logo);
+  emitter.startFollow(logo)
 }
