9c17d2c1aab78f7e4a2c7ecfe43d3c84c825eae3
purxiz <functionofn@gmail.com>
2018-11-07 14:32:23 -0800

    working example of sockets

--- server/server.js
+++ server/server.js
@@ -2,6 +2,20 @@ var express = require('express')
 const app = express()
 const port = 3000
 
+const WebSocket = require('express-ws')
+WebSocket(app)
+
+app.ws('/echo', (ws, req) => {
+  ws.on('message', msg => {
+    ws.send(msg)
+    console.log('message received');
+  })
+
+  ws.on('close', () => {
+    console.log('WebSocket was closed')
+  })
+})
+
 var mysql = require('mysql')
 var connection = mysql.createConnection({
   socketPath: '/var/run/mysqld/mysqld.sock',
@@ -10,15 +24,13 @@ var connection = mysql.createConnection({
   database: 'ghoulgrinder'
 })
 
-connection.connect(function (err) {
+connection.connect( (err) => {
   if (err) {
     console.error(err.stack)
     return;
   }
-  
+
   console.log('connected as id ' + connection.threadId);
 });
-
-app.get('/', (req, res) => res.send("Hello World"))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
