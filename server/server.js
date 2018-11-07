483550c293e9a02ca44d80bd1aa51d1bd3c6079a
purxiz <functionofn@gmail.com>
2018-11-07 13:41:42 -0800

    change tabs to 2 spaces

--- server/server.js
+++ server/server.js
@@ -4,22 +4,20 @@ const port = 3000
 
 var mysql = require('mysql')
 var connection = mysql.createConnection({
-	socketPath	: '/var/run/mysqld/mysqld.sock',
-	user		: 'ghoulgrinder',
-	password 	: 'dev_secret',
-	database 	: 'ghoulgrinder'
+  socketPath: '/var/run/mysqld/mysqld.sock',
+  user: 'ghoulgrinder',
+  password: 'dev_secret',
+  database: 'ghoulgrinder'
 })
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error(err.stack)
     return;
   }
-
+  
   console.log('connected as id ' + connection.threadId);
 });
-
-
 
 app.get('/', (req, res) => res.send("Hello World"))
 
