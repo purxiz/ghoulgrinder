c3b08817b961526bd043861f8e4e94534f69cb40
purxiz <functionofn@gmail.com>
2018-11-08 21:33:15 -0800

    final non-merge commit to master

--- server/server.js
+++ server/server.js
@@ -12,10 +12,13 @@ WebSocket(app)
 //})
 
 ;(async () => {
-  //console.log(await game.getUsers())
+  console.log(await game.getUsers())
 })()
 
-;game.fillUpAccounts(100);
+;(async () => {
+  console.log(await game.getVehicleRouteSteps())
+})()
+
 
 app.ws('/echo', (ws, req) => {
   ws.on('message', (msg) => {
