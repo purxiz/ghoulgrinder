247e200bc3897089497a8288700ad249ad76f4df
purxiz <functionofn@gmail.com>
2018-11-11 13:22:12 -0800

    update documentation for recipe set

--- shared/protocol.js
+++ shared/protocol.js
@@ -1,7 +1,6 @@
 exports.structures = Object.freeze({
   //structures 30-39
   BUILD : 30, //[30, nId, sType]
-  RECIPE_SET : 31,
+  RECIPE_SET : 31, //[31, sId, sRecipe]
   DELETE : 32 //[32, sId]
-  
 })
